950fcf94eb3163a1ae41534d89db207b88341aeb
matthewbestrong <matthewgureshidze@gmail.com>
2024-10-04 19:01:35 +0400

    commit

---
+++ level 1/level 1/level 1/lesson1.py
@@ -0,0 +1,33 @@
+# name = "mate"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნეკობის მიმნიჭებელი სიმბოლო
+# "mate" არის ცვლადისთვის მნიშვნელობა
+
+surname = "gureshidze"
+
+# print(name) 
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "mate" #ეს არის str (string) ტიპის ცვლადი
+age = 15 # ეს არის int ( intenger ) მთელი რიცხვი
+height = 173 #ეს არის float ტიპის ცვლადი ( ათწილადი )
+#booLlean (booL) ტიპის ცვლადი  
+
+knows_programming = True  #True ან FaLse 
+is_ugly = False #snakecase(უბრალოდ წერის სტილი სტანდარტულად) 
+
+isUgly = False # ჯავასკრიპტული cameLcase
+
+
+print(name + " " +  surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print(name + " " + str(age))
