eb0bd2b14eb67e7bfaab864e623a48765789c66d
matthewbestrong <matthewgureshidze@gmail.com>
2024-10-23 21:43:09 +0400

    commit

---
+++ level 06/classwork/main.py
@@ -0,0 +1,29 @@
+print "Hello World"
+print(variable)
+print("Hello World"
+    print("This is an indented line")
+print(undeclared_variable)
+class = 5
+print(class)
+print("Hello World')
+x = 10
+y = "5"
+print(x + y)
+def my_function():
+print("Missing indentation in function")
+1var = 10
+print(1var)
+a, b = 10
+print(a, b)
+print("Unclosed string
+print("Escaped quote: \" )
+True = 5
+print(True)
+# Missing comment symbol
+This is a comment
+if True
+print("No colon in if statement")
+def add(a, b):
+return a + b
+variable
+print Empty statement after variable declaration
---
+++ level 06/homework/level 06/main.py
@@ -0,0 +1,21 @@
+#Camel Case გამოიყენება java, JavaScript, c@, Python
+# SnakeCase გამოიყენება Ruby, PostgreSQL, Python
+# Pascal Case  გამოიყენება java, JavaScript/TypeScript, c#
+#Kebab Case გამოიყენება HTML/CSS, JavaScript
+# Screaming Snake Case Python,Java/C
+#ყველა case რომლებიც გამოიყენება პროგრამირების ენებში
+
+
+
+x = 5; x +=1
+x = 3; x +=3
+x = 4; x +=9
+x = 3; x +=5
+x = 1; x +=7
+x = 2; x +=2
+x = 22; x+=23
+
+
+
+
+
