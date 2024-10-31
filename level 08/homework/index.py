5b5f273c7cbcf78b60d7a9494312bd5255672b75
matthewbestrong <matthewgureshidze@gmail.com>
2024-11-01 00:20:02 +0400

    commit

---
+++ level 08/homework/index.py
@@ -0,0 +1,26 @@
+num1 = 10
+num2 = 7
+num3 = 5
+num4 = 3
+num5 = 6
+
+print((num1 + num2) / num3 - num4 * num5 + num1 ** num3 % num2)
+print(num1 + num2 - num3 * num4 / num5)
+print(num1 * num2 + num3 - num4 ** num5 % num1)
+print((num1 + num2) * num3 - num4 / num5)
+print(num1 ** num2 + num3 - num4 % num5)
+print((num1 % num2 + num3) * num4 / num5)
+print((num1 + num2 * num3) - num4 ** num5)
+print(num1 - num2 + num3 / num4 * num5)
+print(num1 % num2 * num3 + num4 - num5)
+print((num1 + num2) ** num3 - num4 * num5)
+
+
+
+
+
+
+
+
+
+
