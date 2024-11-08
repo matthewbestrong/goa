938bf885b1e821ae7424d2fcbe367033df9ca5f6
matthewbestrong <matthewgureshidze@gmail.com>
2024-11-09 03:28:46 +0400

    commit to main

---
+++ level 10/homework/main.py
@@ -0,0 +1,38 @@
+int("123")
+int(45.6)
+int("0b101", 2)
+int("7E", 16)
+int(3.99) 
+int("003")
+int("-456")
+int(100.0) 
+int("25") + 5
+int(False)
+#str გამოყენების მაგალითები
+str(123)           
+str(45.67)           
+str(True)           
+str(False)           
+str("Hello" + " World") 
+str(int("123"))      
+str(0b101)           
+str([1, 2, 3])       
+str({"key": "value"})  
+str(None)           
+#float გამოყენების მაგალითები
+float("12.34")       
+float(567)          
+float("3.1415")     
+float(True)          
+float(False)        
+float("1e-3")        
+float("-0.456")     
+float("   8.90")    
+float("5") * 2      
+float(int("7"))     
+
+
+
+
+
+
